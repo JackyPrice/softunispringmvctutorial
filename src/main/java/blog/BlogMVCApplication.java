a3997068b53971525701348cce7cc0e0997724e0
Jacky Price <The.Jacky.Price@googlemail.com>
2019-02-04 14:46:33 +0000

    intial commit - all working

---
+++ src/main/java/blog/BlogMVCApplication.java
@@ -0,0 +1,15 @@
+package blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMVCApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BlogMVCApplication.class, args);
+    }
+
+}
+
